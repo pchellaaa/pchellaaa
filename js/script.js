d4680ac3b233fa11928ec5b3e4ac3ad1a4fb2c71
pchellaaa <6klubniks9@gmail.com>
2024-04-26 12:27:05 +0300

    dfdf

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+var swiper = new Swiper(".mySwiper", {  
+    
+    slidesPerView: 1, 
+    navigation: { 
+      nextEl: ".swiper-button-next", 
+      prevEl: ".swiper-button-prev", 
+    }, 
+    pagination: { 
+      el: ".swiper-pagination", 
+    }, 
+    mousewheel: true, 
+    keyboard: true,
+    grabCursor: true,
+  });
+
+
